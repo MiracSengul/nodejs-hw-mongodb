56f9c08b5c11ed44401c78779c170dc28b4d72f7
MiracSengul <mirac.ssengul@hotmail.com>
2025-12-02 22:02:34 +0300

    fixed server

--- src/server.js
+++ src/server.js
@@ -21,12 +21,6 @@ export const setupServer = () => {
     app.use(express.json());
     app.use(cors());
 
-    app.use((req, res, next) => {
-        res.status(404).json({
-            message: "Not Found",
-        });
-    });
-
     app.get("/contacts", async (req, res)=>{
         const contacts =await getAllContacts();
         res.status(200).json({
@@ -49,6 +43,12 @@ export const setupServer = () => {
             message: `Successfully found contact with id ${contactId}!`,
             data: contact,
         })
+    });
+
+    app.use((req, res, next) => {
+        res.status(404).json({
+            message: "Not Found",
+        });
     });
 
     app.use((err, req, res, next) => {
