17cd874f88b32e78a3e6bae7ce132d1f69919160
MiracSengul <mirac.ssengul@hotmail.com>
2025-12-12 14:51:30 +0300

    hw-3

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import pino from "pino-http";
 import cors from "cors";
 import { env } from "./utils/env.js";
-import { getAllContacts, getContact } from "./services/contacts.js"
+import contactsRouter from "./routers/contacts.js"
 
 const PORT = Number(env("PORT", 3000));
 
@@ -21,29 +21,7 @@ export const setupServer = () => {
     app.use(express.json());
     app.use(cors());
 
-    app.get("/contacts", async (req, res)=>{
-        const contacts =await getAllContacts();
-        res.status(200).json({
-            status: 200,
-            message: "Successfully found contacts!",
-            data: contacts,
-        })
-    });
-
-    app.get("/contacts/:contactId", async (req, res) => {
-        const {contactId} = req.params;
-        const contact = await getContact(contactId);
-        if(!contact){
-            res.status(404).json({
-                message: "Contact not found",
-            })
-        }
-        res.status(200).json({
-            status: 200,
-            message: `Successfully found contact with id ${contactId}!`,
-            data: contact,
-        })
-    });
+    app.use(contactsRouter);
 
     app.use((req, res, next) => {
         res.status(404).json({
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,34 @@ export const getContact =  async(id) =>{
     const getContact = await Contacts.findById(id);
     return getContact;
 }
+
+export const createContact = async(payload) => {
+    const contact = Contacts.create(payload);
+    return contact;
+}
+
+export const patchContact = async(contactId, payload, options={}) => {
+    const patchedContact = Contacts.findByIdAndDelete(
+        { _id: contactId },
+        payload,
+        {
+            new: true,
+            includeResultMetadata: true,
+            ...options,
+        }
+    )
+
+    if(!patchedContact || !patchedContact.value){
+        return null;
+    }
+
+    return {
+        contact: contactResult.value,
+        isNew: Boolean(contactResult?.lastErrorObject?.upserted),
+    }
+}
+
+export const deleteContact = async(contactId) => {
+    const contact = await Contacts.findByIdAndDelete({ _id: contactId});
+    return contact;
+}
